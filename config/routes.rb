ac1171f40a10cac827570cb9a5ca4124f28e4812
Louis DEBRAINE <louisdebraine@gmail.com>
2016-12-01 20:56:50 +0100

    Update home and main comportment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
-  get 'pages/main'
+  get 'main', to: 'home#main'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
