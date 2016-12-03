8af2866728013fa60c766a851a055d5b8790c86e
Lucas Benquet <lucasbenquet@ic.home>
2016-12-03 20:15:07 +0100

    Begin Profil intergration page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/profil'
+
   devise_for :users
   root 'home#index'
   get 'main', to: 'home#main'
+  get 'profil', to: 'pages#profil'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
