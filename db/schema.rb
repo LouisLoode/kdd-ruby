73171263fd22df4d2ece12b30a6f3c8b245baade
Louis DEBRAINE <louisdebraine@gmail.com>
2016-12-04 02:38:10 +0100

    update cat and posts crud

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201151834) do
+ActiveRecord::Schema.define(version: 20161204004335) do
 
-  create_table "followers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id"
-    t.integer  "follower"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "cats", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "slug"
+    t.text     "description", limit: 65535
+    t.boolean  "public"
+    t.integer  "parent_id"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "title"
     t.string   "url"
-    t.string   "message"
+    t.text     "message",     limit: 65535
+    t.integer  "category_id"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
   create_table "ranks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
