d431e24ff6a8fd5b2503c4d5588c4eca7af88e4f
Louis DEBRAINE <louisdebraine@gmail.com>
2016-12-05 14:54:02 +0100

    Git push origin dev

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
 
-  
+  def show_by_id
+    @user = User.find(params[:id])
+    @posts = Post.where('user_id' => params[:id])
+  end
+
+
 end
