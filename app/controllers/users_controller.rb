29ba400fc6bea4f95ff457a809af86c1d22457bd
blephy <dolle.allan@gmail.com>
2016-12-06 22:30:08 +0100

    Fix order post in profil - recents first

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   def show
     # id_user = params[:id] === nil ? current_user.id : params[:id]
     @user = User.find(params[:user_id])
-    @posts = Post.where('user_id' => params[:user_id])
+    @posts = Post.where('user_id' => params[:user_id]).sort_by(&:created_at).reverse!
   end
 
   def list
