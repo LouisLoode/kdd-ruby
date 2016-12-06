5475f5d5c969c51e2c5feef337f6c80c6bf86030
blephy <dolle.allan@gmail.com>
2016-12-06 22:04:38 +0100

    Add time since post published in profil view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  helper_method :how_much_time
 
   def show
     # id_user = params[:id] === nil ? current_user.id : params[:id]
@@ -10,4 +11,24 @@ class UsersController < ApplicationController
   def list
   	@users = User.all
   end
+
+  def how_much_time(params)
+    now = Time.new
+    diff = (now - params).to_i
+    if diff < 60
+    	return diff.to_s + " s"
+    elsif diff < (60*60)
+    	return (diff/60).to_s + " m"
+    elsif diff < (60*60*24)
+    	return (diff/(60*60)).to_s + " h"
+    elsif diff < (60*60*24*30)
+    	return (diff/(60*60*24)).to_s + " d"
+    elsif diff < (60*60*24*365)
+    	return (diff/(60*60*24*30)).to_s + " M"
+    elsif diff >= (60*60*24*365)
+    	return (diff/(60*60*24*30*365)).to_s + " Y"
+    end	
+  end  
+
+
 end
