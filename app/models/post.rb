f81cda626ef665e0b90ac0cb919dbf8241827c42
blephy <dolle.allan@gmail.com>
2016-12-09 04:50:43 +0100

    Add action to bottom of post and fix some favorite

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   searchkick autocomplete: [:url, :message]
   belongs_to :user
   has_many :rates
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_and_belongs_to_many :categories
   validates :url, presence: true,
                     length: { minimum: 5 }
