1b57183503d98f5708fbf4e5aec62b3cfe991ebd
Louis DEBRAINE <louisdebraine@gmail.com>
2016-12-09 16:41:23 +0100

    Fix heroku deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,8 +15,13 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # Apache or NGINX already handles this.  
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
