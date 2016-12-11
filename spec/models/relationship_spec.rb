2bdf1bfaa9e4cbeb74f24b4324c8833c304e1a80
blephy <dolle.allan@gmail.com>
2016-12-11 17:21:57 +0100

    Add relationship model rspec

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -7,8 +7,10 @@ RSpec.describe Relationship, :type => :model do
     user2 = User.create(name: "Mordecai", email: "m@rshow.com", password: "testtest", password_confirmation: "testtest")
 
 
-    relation = Relationship.new(followed_id: user.id, follower_id: user2.id)
-    expect(relation.valid?).to eq(true)
+    relation = Relationship.new(follower_id: user.id, followed_id: user2.id)
+    relation.save
+    expect(relation.follower_id).to eq(user.id)
+    expect(relation.followed_id).to eq(user2.id)
   end
 
 end
