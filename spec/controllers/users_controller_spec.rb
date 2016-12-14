2a3fefa68e48b3cad0338cd8ce99c7240e573e82
blephy <dolle.allan@gmail.com>
2016-12-14 21:52:38 +0100

    Add users controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -15,4 +15,38 @@ RSpec.describe UsersController, :type => :controller do
       expect(response.body).to include("Followers")
     end
   end
+
+  describe "GET followers" do
+    it "can show user followers" do
+      eileen = User.create(name: "Eileen #{rand(1000)}", email: "Eileen@rs.com", password: "testtest", password_confirmation: "testtest")
+      user = User.create(name: "NewUser #{rand(1000)}", email: "Eildsdsdsseen@rs.com", password: "testtest", password_confirmation: "testtest")
+      sign_in user
+
+      last_relation = Relationship.create(follower_id: user.id, followed_id: eileen.id)
+
+      expect(last_relation.follower_id).to eq(user.id)
+      expect(last_relation.followed_id).to eq(eileen.id)
+
+      get :followers, params: {id: eileen.id}
+
+      expect(response.body).to include("#{user.name}")
+    end
+  end
+
+  describe "GET followings" do
+    it "can show user followings" do
+      eileen = User.create(name: "Eileen #{rand(1000)}", email: "Eileen@rs.com", password: "testtest", password_confirmation: "testtest")
+      user = User.create(name: "NewUser #{rand(1000)}", email: "Eildsdsdsseen@rs.com", password: "testtest", password_confirmation: "testtest")
+      sign_in user
+
+      last_relation = Relationship.create(follower_id: user.id, followed_id: eileen.id)
+
+      expect(last_relation.follower_id).to eq(user.id)
+      expect(last_relation.followed_id).to eq(eileen.id)
+
+      get :following, params: {id: user.id}
+
+      expect(response.body).to include("#{eileen.name}")
+    end
+  end
 end
