65933449d86053b061265e9c7b9d2ebe32af17ac
blephy <dolle.allan@gmail.com>
2016-12-17 13:53:26 +0100

    Change some redirect 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActionController::RoutingError, with: :render_404
   helper_method :how_much_time
 
