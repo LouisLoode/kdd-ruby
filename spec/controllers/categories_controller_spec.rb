90432dcbbf76fdc772ab207136ab905c2c563f1c
blephy <dolle.allan@gmail.com>
2016-12-17 17:03:28 +0100

    Add possibility to create categorie and rspec it

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -18,4 +18,22 @@ RSpec.describe CategoriesController, :type => :controller do
 
     end
   end
+
+
+    describe "POST create" do
+      it "can create a categorie" do
+        eileen = User.create(name: "Eileen #{rand(1000)}", email: "Eileen@rs.com", password: "testtest", password_confirmation: "testtest")
+        sign_in eileen
+
+        post :create, params: { category: {name: "MyNewCategorie", description: "Something", parent_id: nil}}
+
+        categorie = Category.last
+
+        expect(categorie.name).to eq("MyNewCategorie")
+        expect(categorie.description).to eq("Something")
+        expect(categorie.parent_id).to eq(nil)
+        expect(categorie.public).to include(0)
+        
+      end
+    end
 end
