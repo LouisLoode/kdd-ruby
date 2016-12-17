e354dd4a3593b34e89557c4ac814412e57a656b4
blephy <dolle.allan@gmail.com>
2016-12-17 14:43:16 +0100

    Finish rspec test and add security to search possibility

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,6 @@
 class SearchController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @results = Searchkick.search params[:query], index_name: [Post, Category, User]
     @query = params[:query]
